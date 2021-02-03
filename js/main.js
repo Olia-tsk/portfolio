74fd2f4bf9eef309275af0820479b3152c854959
Olia <kurbatova.olia@gmail.com>
2021-02-03 15:52:07 +0700

    add show/hide effect for image

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-    console.log('working')
+    // Показать-скрыть пример
+    var btnShow = $('.button__show');
+    var btnHide = $('.button__hide');
+    var image = $('.advantages-example');
+
+    btnShow.on('click', showImage);
+    btnHide.on('click', hideImage);
+
+    function showImage(event) {
+        event.preventDefault();
+        btnShow.addClass('advantages-button--hidden');
+        btnHide.removeClass('advantages-button--hidden');
+        image.slideDown(500, "linear");
+    }
+
+    function hideImage(event) {
+        event.preventDefault();
+        btnShow.removeClass('advantages-button--hidden');
+        btnHide.addClass('advantages-button--hidden');
+        image.slideUp(500, "linear");
+    }
 });
